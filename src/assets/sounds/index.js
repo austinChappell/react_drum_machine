fa565e649b9886dcb155e36cdf873941f2a7998e
austinChappell <Austin.W.Chappell@gmail.com>
2018-10-29 08:28:32 -0500

    use mp3 instead of wav

--- src/assets/sounds/index.js
+++ src/assets/sounds/index.js
@@ -1,12 +1,12 @@
-import china from './china.wav';
-import crash from './crash.wav';
-import hhClosed from './hi_hat_closed.wav';
-import hhOpen from './hi_hat_open.wav';
-import kick from './kick.wav';
-import rideBell from './ride_bell.wav';
-import ride from './ride.wav';
-import snareRim from './snare_rim.wav';
-import snare from './snare.wav';
+import china from './china.mp3';
+import crash from './crash.mp3';
+import hhClosed from './hi_hat_closed.mp3';
+import hhOpen from './hi_hat_open.mp3';
+import kick from './kick.mp3';
+import rideBell from './ride_bell.mp3';
+import ride from './ride.mp3';
+import snareRim from './snare_rim.mp3';
+import snare from './snare.mp3';
 
 export default {
   china,
